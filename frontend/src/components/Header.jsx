1f2698fcead1e3b02f6b62c169675c720a615892
ahmad adib <ahmadadibsyaifulloh@gmail.com>
2025-02-17 16:01:02 +0700

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,33 +7,33 @@ import {HiUserCircle} from 'react-icons/hi'
 const Header = () => {
 
   return (
-    <div className='flex items-center justify-between bg-white py-2 px-8 md:px-10 drop-shadow-md'>
+    <div className='flex items-center justify-between bg-white py-2 px-8 md:px-16 drop-shadow-md'>
       {/* logo */}
       <Link to={'/'} >
         <div className='flex items-center gap-2 hover:scale-90 transition-all'>
             <img src={assets.logo} className='w-[55px] h-[55px] ' alt="logo" />
-            <span className='font-serif text-2xl'>JustiBot</span>
+            {/* <span className='font-serif text-2xl'>JustiBot</span> */}
         </div>
       </Link>
 
       {/* Navlink */}
-      <ul className='hidden xl:flex items-center gap-10 font-semibold text-base'>
-        <li className='p-3 hover:bg-[#B9AB99] rounded-full transition-all cursor-pointer'>
+      <ul className='hidden lg:flex  items-center gap-6 font-serif text-base'>
+        <li className='px-3 py-1.5 hover:bg-[#B9AB99] rounded-full transition-all cursor-pointer'>
           <NavLink to={'/'}>
             <p>Beranda</p>
           </NavLink>
         </li>
-        <li className='p-3 hover:bg-[#B9AB99] rounded-full transition-all cursor-pointer'>
+        <li className='px-3 py-1.5 hover:bg-[#B9AB99] rounded-full transition-all cursor-pointer'>
           <NavLink to={'/chatbot'}>
             <p>Konsultasi AI</p>
           </NavLink>
         </li>
-        <li className='p-3 hover:bg-[#B9AB99] rounded-full transition-all cursor-pointer'>
+        <li className='px-3 py-1.5 hover:bg-[#B9AB99] rounded-full transition-all cursor-pointer'>
           <NavLink to={'/document'}>
             <p>Dokumen Hukum</p>
           </NavLink>
         </li>
-        <li className='p-3 hover:bg-[#B9AB99] rounded-full transition-all cursor-pointer'>
+        <li className='px-3 py-1.5 hover:bg-[#B9AB99] rounded-full transition-all cursor-pointer'>
           <NavLink to={'/lawyer'}>
             <p>Direktori Pengacara</p>
           </NavLink>
@@ -42,8 +42,8 @@ const Header = () => {
 
       {/* button login/signup */}
       <Link to=''>
-        <Button className='bg-[#B9AB99] text-black font-semibold rounded-full'>
-          <HiUserCircle className="mr-2 h-6 w-5 " />
+        <Button className=' bg-[#B9AB99] text-black font-serif rounded-full w-36'>
+          <HiUserCircle className="mr-2 h-5 w-5 " />
           Login / SignUp
         </Button>
       </Link>
