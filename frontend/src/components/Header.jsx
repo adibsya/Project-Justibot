bd0616a15c0f8553ae41d8ed00502c52e359f189
ahmad adib <ahmadadibsyaifulloh@gmail.com>
2025-02-19 17:54:19 +0700

    update navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,15 +11,15 @@ const Header = () => {
 
   return (
     <div>
-      <header className="bg-[#612A22] fixed top-0 left-0 w-full z-50 shadow-md font-medium">
+      <header className="bg-[#612A22] fixed top-0 left-0 w-full z-50 shadow-md text-lg">
         <nav className="container mx-auto px-2 py-3 flex items-center justify-between">
           <div className="text-white font-bold">
             <Link to={'/'}>
-              <img src={assets.logo} alt="Logo" className="w-16" />
+              <img src={assets.logo} alt="Logo" className="w-14" />
             </Link>
           </div>
           <div className="hidden md:flex items-center space-x-8">
-            <ul className="flex items-center space-x-8 text-white ">
+            <ul className="flex items-center space-x-8 text-white">
               <li>
                 <NavLink to='/' className='transition duration-300 ease-in-out hover:text-gray-300 relative after:block after:h-0.5 after:bg-white after:w-0 after:transition-all after:duration-300 hover:after:w-full'>
                   <p>Home</p>
@@ -41,18 +41,18 @@ const Header = () => {
                 </NavLink>
               </li>
             </ul>
-            <Link to='/login' className='flex justify-between ml-4 px-4 py-2 bg-[#B9AB99] text-black rounded-full transition duration-300 ease-in-out hover:bg-gray-200'>
+            <Link to='/login' className='flex justify-between ml-4 px-4 py-2 bg-[#B9AB99] text-black rounded-full transition duration-300 ease-in-out hover:bg-gray-100 hover:scale-90'>
                 <img src={assets.user_icon} className='w-4 mr-2'/>
-                <p>Login/Signup</p>
+                <p>Login / Signup</p>
             </Link>
             
           </div>
           <div className="md:hidden flex items-center">
-           <Link to='/login' className='flex justify-between ml-4 px-4 py-2 bg-[#B9AB99] text-black rounded-full'>
+           <Link to='/login' className='flex justify-between ml-4 px-4 py-2 bg-[#B9AB99] text-black rounded-full hover:bg-gray-100 hover:scale-90'>
                 <img src={assets.user_icon} className='w-4 mr-2'/>
-                <p>Login/Signup</p>
+                <p>Login / Signup</p>
             </Link>
-            <button onClick={toggleMenu} className="outline-none ml-6">
+            <button onClick={toggleMenu} className="outline-none ml-6 mr-3">
               <svg className="w-6 h-6 text-white" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                 {isMenuOpen ? (
                   <path d="M6 18L18 6M6 6l12 12"></path>
