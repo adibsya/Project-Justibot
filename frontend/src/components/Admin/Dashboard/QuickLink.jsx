eeee2003bb85a42856e024ce0ba5f7d5cb88bd90
TasyaDwiyantii <tasyady02@gmail.com>
2025-04-19 19:36:56 +0700

    tasya

--- frontend/src/components/Admin/Dashboard/QuickLink.jsx
+++ frontend/src/components/Admin/Dashboard/QuickLink.jsx
@@ -4,13 +4,13 @@ import assets from '../../../assets/assets';
 
 const QuickLink = () => {
   return (
-    <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-200 w-full max-w-xs">
-      <div className="mb-4">
-        <h2 className="text-xl font-bold text-[#122E40]">Documentation</h2>
+    <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-200 ">
+      <div className="mb-6">
+        <h2 className="text-2xl font-bold text-[#122E40]">Documentation</h2>
       </div>
 
       <Link to="/admin/documentation" className="block group">
-        <div className="bg-[#F9F8F5] hover:bg-[#E5DED5] transition-all duration-200 p-4 rounded-xl flex items-center gap-4 border border-gray-100 shadow-sm group-hover:shadow-md">
+        <div className="bg-[#F9F8F5] hover:bg-[#E5DED5] transition-all duration-200 p-6 rounded-xl flex items-center gap-4 border border-gray-100 shadow-sm group-hover:shadow-md">
           <div className="w-12 h-12 bg-[#A69C7A]/10 rounded-full flex items-center justify-center">
             <img src={assets.logo_dokumen} alt="file icon" className="w-6 h-6" />
           </div>
--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import QuickLinks from "../../components/Admin/Dashboard/QuickLink";
 import Statistics from "../../components/Admin/Dashboard/Statistic";
 import Chart from "../../components/Admin/Dashboard/Chart";
-import { div } from "framer-motion/client";
 
 const Dashboard = () => {
   return (
-    <div>
-      <div className="p-6 flex">
+    <div className="p-6 space-y-6">
+      <div className="flex gap-6 items-stretch">
         <div className="flex-1">
           <Statistics />
-          <Chart />
         </div>
-        <div className="w-72 ml-6">
+        <div className="w-72">
           <QuickLinks />
         </div>
+      </div>
+
+      <div>
+        <Chart />
       </div>
     </div>
   );
